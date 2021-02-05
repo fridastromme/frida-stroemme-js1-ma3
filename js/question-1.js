e9aba86b349a2a5e6ea609dc50bc3a7544a44305
Fridastromme <fridastromme@hotmail.com>
2021-02-05 18:24:50 +0100

    1 Assignment

---
+++ js/question-1.js
@@ -0,0 +1,7 @@
+// Question 1
+
+const divide = (a, b) => a % b;
+
+const result = divide(7, 3);
+
+console.log(result);
---
+++ js/question-2.js
@@ -0,0 +1,20 @@
+// Question 2
+
+const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating";
+
+const gamesContainer = document.querySelector(".games");
+
+async function getGames() {
+    const response = await fetch(url);
+    const results = await response.json();
+    gamesContainer.innerHTML = "";
+
+    for (let i = 0; i < games.length; i++) {
+        if (i === 8) {
+            break;
+        }
+
+        gamesContainer.innerHTML += `<div class="games">${games[i]}</div>`
+    }
+}
+getGames();
