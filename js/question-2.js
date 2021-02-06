5bcc66d7424fff0e0709c015957985efd793e2c7
Fridastromme <fridastromme@hotmail.com>
2021-02-06 14:34:55 +0100

    Assignment 2

--- js/question-2.js
+++ js/question-2.js
@@ -1,21 +1,40 @@
-// Question 2 
-// Sjekk ut lessonen om denne en gang til : )))))
+// // Question 2 
+// // Sjekk ut lessonen om denne en gang til : )))))
 
 const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating";
 
 const gamesContainer = document.querySelector(".games");
 
-async function getGames() {
-    const response = await fetch(url);
-    const results = await response.json();
-    gamesContainer.innerHTML = "";
+ async function getGames() {
 
-    for (let i = 0; i < games.length; i++) {
+     const response = await fetch(url);
+
+     const data = await response.json();
+
+     const games = data.results;
+
+     gamesContainer.innerHTML = "";
+
+     for(let i = 0; i < games.length; i++) {
+         const nameContainer = games[i].name;
+         const ratingContainer = games[i].rating;
+         const tagsContainer = games[i].tags.length;
+
         if (i === 8) {
-            break;
-        }
+             break;
+         }
 
-        gamesContainer.innerHTML += `<div class="games">${games[i]}</div>`
-    }
-}
+         gamesContainer.innerHTML += `<div class="name">${nameContainer}</div><div class="rating">${ratingContainer}</div><div class="name">${tagsContainer}</div>`
+     }
+ }
+
 getGames();
+
+try {
+    getGames();
+}
+catch(error){
+
+    const errorMessage = "Could not load games. Try again later."
+    gamesContainer.innerHTML += `<h2>${errorMessage}</h2>`;
+}
